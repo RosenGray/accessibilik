e7a9a11a3c6574b24ad8f9b9034e8087363e8e1f
Vladislav Iokhim <vladislaviokhim@Vladislavs-MacBook-Pro.local>
2024-02-13 21:59:57 +0200

    zindex

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const textTags = ["h1","h2","h3","h4","h5","h6","span","p","a","label","i","button","img","ol","svg"]
 export const ACC_MENU_CONTAINER_ID = "acc-accessibility-menu";
 export const APP_ID = "accessibilik";
-export const PORTAL_APP_ID = `portal-${APP_ID}`;
+export const PORTAL_APP_ID = `root-${APP_ID}`;
