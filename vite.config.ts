dc265664aa814030a89031d6e3ce16b5cac23658
Vladislav Iokhim <vladislaviokhim@Vladislavs-MacBook-Pro.local>
2024-02-13 22:00:16 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -10,72 +10,33 @@ import { terser } from "rollup-plugin-terser";
 
 // import { visualizer } from 'rollup-plugin-visualizer';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-    // terser({
-    //   format: {
-    //     comments: false,
-    //     beautify: false,
-    //   },
-    //   compress: {
-    //     passes: 2, // More passes over the code for compression
-    //     drop_console: true, // Remove console statements
-    //     sequences: true, // Join consecutive simple statements using the “comma operator”
-    //   },
-    //   mangle: true, // Shorten variable names
-    // }),
-  ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, "lib/main.tsx"),
-      name: "accessibilik",
-      formats: ["umd"],
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        // Provide global variables to use in the UMD build
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
-  },
-});
-
 // export default defineConfig({
 //   plugins: [
 //     react(),
 //     svgr(),
-//     libInjectCss(),
-//     dts({ include: ["lib"], copyDtsFiles: true }),
-//     terser({
-//       format: {
-//         comments: false,
-//         beautify: false,
-//       },
-//       compress: {
-//         passes: 2, // More passes over the code for compression
-//         drop_console: true, // Remove console statements
-//         sequences: true, // Join consecutive simple statements using the “comma operator”
-//       },
-//       mangle: true, // Shorten variable names
-//     }),
+//     // terser({
+//     //   format: {
+//     //     comments: false,
+//     //     beautify: false,
+//     //   },
+//     //   compress: {
+//     //     passes: 2, // More passes over the code for compression
+//     //     drop_console: true, // Remove console statements
+//     //     sequences: true, // Join consecutive simple statements using the “comma operator”
+//     //   },
+//     //   mangle: true, // Shorten variable names
+//     // }),
 //   ],
 //   build: {
 //     lib: {
-//       entry: resolve(__dirname, "lib/main.ts"),
-//       formats: ["es"],
+//       entry: resolve(__dirname, "lib/main.tsx"),
+//       name: "accessibilik",
+//       formats: ["umd"],
 //     },
-//     copyPublicDir: false,
 //     rollupOptions: {
 //       external: ["react", "react-dom"],
 //       output: {
-//         assetFileNames: "assets/[name][extname]",
-//         entryFileNames: "[name].js",
+//         // Provide global variables to use in the UMD build
 //         globals: {
 //           react: "React",
 //           "react-dom": "ReactDOM",
@@ -84,3 +45,42 @@ export default defineConfig({
 //     },
 //   },
 // });
+
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr(),
+    libInjectCss(),
+    dts({ include: ["lib"], copyDtsFiles: true }),
+    terser({
+      format: {
+        comments: false,
+        beautify: false,
+      },
+      compress: {
+        passes: 2, // More passes over the code for compression
+        drop_console: true, // Remove console statements
+        sequences: true, // Join consecutive simple statements using the “comma operator”
+      },
+      mangle: true, // Shorten variable names
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+    copyPublicDir: false,
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
