b92a98aacaac3283caf4ebb5e3ac9bd2a4f45ffe
RosenGray <vladonchik@gmail.com>
2026-02-05 20:50:35 +0200

    D

--- lib/components/Accessibilik/index.tsx
+++ lib/components/Accessibilik/index.tsx
@@ -11,7 +11,7 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import Portal from "../Portal/Portal";
 import i18n from "i18next";
 import { ChangeAccDraftHander } from "../../types";
-import { getAccInitState, registerDomain } from "../../utils";
+import { getAccInitState } from "../../utils";
 import { initReactI18next } from "react-i18next";
 import {
   Resources,
